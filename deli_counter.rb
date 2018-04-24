9f328ece748dbc0d130d4b079a41ea6f9e2f0d06
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 23:14:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,10 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length > 0
+    line = ["The line is currently: "]
     katz_deli.each_with_index do |name, index|
-  puts "The line is currently: #{index+1}. #{name} "
+    line << "#{index+1}. #{name} "
+    line.join
 end
 else
   puts "The line is currently empty."
