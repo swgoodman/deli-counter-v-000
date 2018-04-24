717c2ec978457be1f87ead59fda6a4771a6b70d6
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 23:32:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,12 +19,16 @@ end
 
 def take_a_number(katz_deli, name)
     katz_deli << "#{name}"
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+      puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
 
 
 def now_serving(katz_deli)
+  if katz_deli.length > 0
+    puts
+  else
+    puts "There is nobody waiting to be served!"
 
 end
