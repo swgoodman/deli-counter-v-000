cdaccdecd6f62c79316f5796ed21692b377b4289
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 23:29:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def take_a_number(katz_deli, name)
     katz_deli << "#{name}"
-    puts "Welcome, #{name}. You are number #{katz_deli.index + 1} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.index+1} in line."
 
 end
 
