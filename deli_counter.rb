d494a7904fdacf3cde1fd8e15fdcd8ab68818886
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 23:02:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length > 0
-  puts "The line is currently:  #{array[0]}"
+  puts "The line is currently:  #{katz_deli[0]}"
 else
   puts "The line is currently empty."
 end
