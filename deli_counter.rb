904ab7959b2bf6b03990c52eb11be28096496b98
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 23:33:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,7 +27,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length > 0
-    puts "Currently serving #{katz_deli.first}"
+    puts "Currently serving #{katz_deli.first}."
   else
     puts "There is nobody waiting to be served!"
   end
