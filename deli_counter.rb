c73f0f1af1ead25f4b8f4aa49daf4b58198ecf51
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 23:11:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(katz_deli)
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
   puts "The line is currently: #{index+1}. #{name} "
+end
 else
   puts "The line is currently empty."
 end
